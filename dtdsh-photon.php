a826ed78e0c625c002e1cabce7e3cee0d62e5600
kazuki-nisshin <nisshin@dtdsh.com>
2016-11-11 23:50:25 +0900

    First commit.

---
+++ dtdsh-photon.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: dtdsh Photon
+ * Description: Jetpack Photon Customized by Nisshin.
+ * Version: 1.0.0
+ * Plugin URI: https://dtdsh.com/plugins/dtdsh-photon/
+ * Author: Nisshin Printing Inc.
+ * Author URI: https://dtdsh.com/
+ * Domain Path: /languages
+ * Text Domain: dtdsh-photon
+ */
